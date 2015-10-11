5c22831a4f68c6f24ce602635699876c022b12f0
QDPeng <liusp_008@163.com>
2015-10-11 17:46:01 +0800

    add baseFragment

--- app/src/main/java/com/lsp/test/MainActivity.java
+++ app/src/main/java/com/lsp/test/MainActivity.java
@@ -13,7 +13,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.lsp.test.Util.ToastUtil;
+import com.lsp.test.util.ToastUtil;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
