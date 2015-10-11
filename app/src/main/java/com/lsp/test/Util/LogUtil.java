bd3b8a9a05d6489279056099b49c457548a84e8e
QDPeng <liusp_008@163.com>
2015-10-11 17:36:46 +0800

    Initial commit

---
+++ app/src/main/java/com/lsp/test/Util/LogUtil.java
@@ -0,0 +1,21 @@
+package com.lsp.test.Util;
+
+import android.util.Log;
+
+/**
+ * Created by Administrator on 2015/10/3.
+ */
+public class LogUtil {
+    public static boolean isDebug = true;
+    public static final String TAG = "fc51";
+
+    public static void i(String title, String info) {
+        if (isDebug)
+            Log.i(TAG, title + "--->" + info);
+    }
+
+    public static void e(String title, String info) {
+        if (isDebug)
+            Log.e(TAG, title + "--->" + info);
+    }
+}
