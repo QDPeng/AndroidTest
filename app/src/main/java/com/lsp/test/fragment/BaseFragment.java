d4977e495166ceba34b15505ea641ae0cb48e392
QDPeng <liusp_008@163.com>
2015-10-11 17:46:28 +0800

    add baseFragment

---
+++ app/src/main/java/com/lsp/test/fragment/BaseFragment.java
@@ -0,0 +1,19 @@
+package com.lsp.test.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2015/10/11.
+ */
+public class BaseFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
